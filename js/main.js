3d75aa9951c48bfed2f2ab4da96200b29a220038
Fubuki-World0510 <99785674+Fubuki-World0510@users.noreply.github.com>
2022-12-17 22:37:01 +0700

    Add animation "Loading Screen"

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ window.addEventListener('scroll', function () {
     var header = document.querySelector('topbar');
     header.classList.toggle('sticky', window.scrollY > 0);
 });
+//Loading Screen
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader-hidden");
+
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader")
+    })
+})
