3a84d107e21ba4ad5aea792a3521ded45774b2f7
QuinVN <gdben221008@gmail.com>
2022-12-19 20:44:57 +0700

    add loading screen

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
-//Topbar On Scroll
-window.addEventListener('scroll', function () {
-    var header = document.querySelector('topbar');
-    header.classList.toggle('sticky', window.scrollY > 0);
-});
 //Loading Screen
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.loader');
 
-    loader.classList.add("loader-hidden");
+    loader.classList.add('loader-hidden');
 
-    loader.addEventListener("transitionend", () => {
-        document.body.removeChild("loader")
-    })
-})
+    loader.addEventListener('transitionend', () => {
+        document.body.removeChild('loader');
+    });
+});
