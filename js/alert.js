1e7de6507ef0210277c0f28dd9b9894b41f5644c
QuinVN <gdben221008@gmail.com>
2022-12-20 23:31:26 +0700

    restructure files

---
+++ js/alert.js
@@ -0,0 +1,51 @@
+function notify(type, message) {
+    (() => {
+        var area = document.getElementById('notification-area');
+        let n = document.createElement('div');
+        let notification = Math.random().toString(36).substr(2, 10);
+        n.setAttribute('id', notification);
+        n.classList.add('notification', type);
+        n.innerHTML = '<div><b>Thông báo</b></div>' + message;
+        area.appendChild(n);
+
+        let color = document.createElement('div');
+        let colorid = 'color' + Math.random().toString(36).substr(2, 10);
+        color.setAttribute('id', colorid);
+        color.classList.add('notification-color', type);
+        document.getElementById(notification).appendChild(color);
+
+        let icon = document.createElement('a');
+        let iconid = 'icon' + Math.random().toString(36).substr(2, 10);
+        icon.setAttribute('id', iconid);
+        icon.classList.add('notification-icon', type);
+        document.getElementById(notification).appendChild(icon);
+
+        let _icon = document.createElement('i');
+        let _iconid = '_icon' + Math.random().toString(36).substr(2, 10);
+        _icon.setAttribute('id', _iconid);
+
+        if (type == 'success') {
+            _icon.className = 'fa fa-2x fa-check-circle';
+        } else {
+            _icon.className = 'fa fa-2x fa-exclamation-circle';
+        }
+        document.getElementById(iconid).appendChild(_icon);
+
+        area.style.display = 'block';
+        setTimeout(() => {
+            var notifications = document
+                .getElementById('notification-area')
+                .getElementsByClassName('notification');
+            for (let i = 0; i < notifications.length; i++) {
+                if (notifications[i].getAttribute('id') == notification) {
+                    notifications[i].remove();
+                    break;
+                }
+            }
+
+            if (notifications.length == 0) {
+                area.style.display = 'none';
+            }
+        }, 5000);
+    })();
+}
